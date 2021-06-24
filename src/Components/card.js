90862f2ec9d615a32e02e7ebb8642af4019e1660
ChristineAu-yeung <auye4760@mylaurier.ca>
2021-06-24 18:37:33 -0400

    Fixed indentation

--- src/Components/card.js
+++ src/Components/card.js
@@ -10,35 +10,35 @@ import HowlingAbyss from '../imgs/Howling_abyss_icon.png';
 
 function Card(props) {
 
-    const gameMode = {
-        'ARAM': HowlingAbyss,
-        'CLASSIC': SummonerRift,
-        'ONEFORALL': SummonerRift,
-				'NEXUSBLITZ': SummonerRift
-    }
+	const gameMode = {
+		'ARAM': HowlingAbyss,
+		'CLASSIC': SummonerRift,
+		'ONEFORALL': SummonerRift,
+		'NEXUSBLITZ': SummonerRift
+	}
 
-    const gameRole = {
-        'MIDDLE': middle,
-        'TOP': top,
-        'BOTTOM': bottom,
-        'SUPPORT': support,
-        'JUNGLE': jungle,
-        'DUO_SUPPORT': support,
-        'DUO_CARRY': bottom,
-        'NONE': fill,
-        'DUO': fill
-    }
+	const gameRole = {
+		'MIDDLE': middle,
+		'TOP': top,
+		'BOTTOM': bottom,
+		'SUPPORT': support,
+		'JUNGLE': jungle,
+		'DUO_SUPPORT': support,
+		'DUO_CARRY': bottom,
+		'NONE': fill,
+		'DUO': fill
+	}
 
-    return (
-        <div className={`Card ${props.data.winLose ? 'gameWin':'gameLose'}`}>
-            <div>
-                <img className={'lane-icon'} src={`http://ddragon.leagueoflegends.com/cdn/11.9.1/img/champion/${props.data.champInfo}`} alt="champ-icon"/>
-                <div className={'kda'}>{` ${props.data.kills} / `} <span className='deaths-font'>{props.data.deaths}</span> {` / ${props.data.assists}`}</div>
-                <img className={'lane-icon'} src={gameMode[props.data.gameMode]} alt="lane"/>
-                <img className={'lane-icon'} src={props.data.gameMode !== 'ARAM' ? gameRole[props.data.lane]: gameRole['NONE'] } alt="role"/>
-            </div>
-        </div>
-    )
+	return (
+		<div className={`Card ${props.data.winLose ? 'gameWin' : 'gameLose'}`}>
+			<div>
+				<img className={'lane-icon'} src={`http://ddragon.leagueoflegends.com/cdn/11.9.1/img/champion/${props.data.champInfo}`} alt="champ-icon" />
+				<div className={'kda'}>{` ${props.data.kills} / `} <span className='deaths-font'>{props.data.deaths}</span> {` / ${props.data.assists}`}</div>
+				<img className={'lane-icon'} src={gameMode[props.data.gameMode]} alt="lane" />
+				<img className={'lane-icon'} src={props.data.gameMode !== 'ARAM' ? gameRole[props.data.lane] : gameRole['NONE']} alt="role" />
+			</div>
+		</div>
+	)
 }
 
 export default Card;
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -5,38 +5,38 @@ import Card from './card'
 const apiKey = process.env.API_KEY
 
 function Navbar() {
-    const [username, setUserName] = useState("");
-    const [data, setData] = useState([]);
-    const handleSearch = async (value) => {
-        if(!value || value === "") return alert("Please enter a valid username"); 
-        // console.log(value)
-				let url = `http://localhost:5000?summonerName=${value}`
-        const res = await axios.get(url)
-        console.log(res)
-        if(res.status !== 200 || !Array.isArray(res.data)) return alert("Please enter a valid username"); 
-        setData(res.data)
-    }
+	const [username, setUserName] = useState("");
+	const [data, setData] = useState([]);
+	const handleSearch = async (value) => {
+		if (!value || value === "") return alert("Please enter a valid username");
+		// console.log(value)
+		let url = `http://localhost:5000?summonerName=${value}`
+		const res = await axios.get(url)
+		console.log(res)
+		if (res.status !== 200 || !Array.isArray(res.data)) return alert("Please enter a valid username");
+		setData(res.data)
+	}
 
-    return (
-        <div className={'navbar'}>
-            <div className={'title'}>
-                Esports History <center className={'nameLeague'}>League of Legends</center>
-            </div>
-            <div className={'userName'}>
-                <textarea className={'userTextArea'} placeholder="Enter username" value={username} onChange={e => setUserName(e.target.value)}>
-                </textarea>
-                <button className={'searchButton'} value = 'Search' onClick={(e) => handleSearch(username)}>
-                   Search
-                </button>
-            </div>
-            <div> 
-                {data.map(entry =>
-                    // console.log(entry) {`${entry.winLose}`}
-                    <Card data={entry}/>
-                )}
-            </div>
-        </div>
-    );
+	return (
+		<div className={'navbar'}>
+			<div className={'title'}>
+				Esports History <center className={'nameLeague'}>League of Legends</center>
+			</div>
+			<div className={'userName'}>
+				<textarea className={'userTextArea'} placeholder="Enter username" value={username} onChange={e => setUserName(e.target.value)}>
+				</textarea>
+				<button className={'searchButton'} value='Search' onClick={(e) => handleSearch(username)}>
+					Search
+				</button>
+			</div>
+			<div>
+				{data.map(entry =>
+					// console.log(entry) {`${entry.winLose}`}
+					<Card data={entry} />
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default Navbar;
